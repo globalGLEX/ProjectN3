7aab392819c77bd9d0245c80aedcc6680545cf5a
globalGLEX <priitkru@gmail.com>
2025-12-11 15:41:43 +0200

    make categories and restaurants scrollable

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,8 +8,30 @@ function Categories() {
                 <div className="category-item">SUSHI</div>
                 <div className="category-item">PIZZA</div>
             </div>
+            
         </div>
     );
   }
+ /*  function CategoriesCarousel() {
+    return (
+            
+        <div className="categories-carousel">
+            <ul>
+            <li>
+                <h2>Page 1</h2>
+            </li>
+            <li>
+                <h2>Page 2</h2>
+            </li>
+            <li>
+                <h2>Page 3</h2>
+            </li>
+            <li>
+                <h2>Page 4</h2>
+            </li>
+            </ul>
+        </div>
+    );
+  }  */
   export default Categories
   
