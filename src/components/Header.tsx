996ce147ed186a0aaafa585f005b0a7db17f462a
globalGLEX <priitkru@gmail.com>
2026-02-09 16:44:33 +0200

    improve shopping cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,12 @@ import cart from '../assets/cart32.png';
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
+
+interface CartContentItemProps {
+  amount: number;
+  productName: string;
+  productPrice: number;
+}
 function Header() {
   
   return (
@@ -47,23 +53,7 @@ export function Cart({onClose}){
     <div className="cart-modal">
     <button className="cart-close-button" onClick={onClose} autoFocus>X</button>
       <h2>Your order</h2>
-    <div className="cart-content">
-      <p> Product1 - amount - price </p>
-      <p> Product2 - amount - price </p>
-      <p> Product3 - amount - price </p>
-      <p> Product1 - amount - price </p>
-      <p> Product2 - amount - price </p>
-      <p> Product3 - amount - price </p>
-      <p> Product1 - amount - price </p>
-      <p> Product2 - amount - price </p>
-      <p> Product3 - amount - price </p>
-      <p> Product1 - amount - price </p>
-      <p> Product2 - amount - price </p>
-      <p> Product3 - amount - price </p>
-      <p> Product1 - amount - price </p>
-     
-    
-    </div>
+    <CartContent />
     <button className="checkout-button">Checkout</button>
     </div>
     </>
@@ -72,6 +62,26 @@ export function Cart({onClose}){
 }
 function CartBackdrop( {onClose} ) {
   return <div className="backdrop" onClick={onClose}/>
+}
+
+function CartContent() {
+  return (
+  <>
+  <div className="cart-content">
+    <CartContentItem amount={1} productName="Cheeseburger" productPrice={3}/>
+    <CartContentItem amount={1} productName="Fries" productPrice={3}/>
+    
+  </div>
+  <div className="cart-total"><p> <b>Total: 12 € </b></p></div>
+  </>)
+}
+function CartContentItem(props: CartContentItemProps) {
+  return (
+    <>
+  <p> x{props.amount} - <b>{props.productName}</b> - <b>{props.productPrice}€</b> </p>
+  <p className="cart-option-line"> • without buns</p>
+  </>
+)
 }
 export default Header
 
